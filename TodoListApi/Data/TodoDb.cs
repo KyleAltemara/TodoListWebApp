f7a885266ea82bef0f79c902556ded09e1d529a9
Kyle <Kyle.Altemara@gmail.com>
2024-09-18 09:06:16 -0700

    Tested Get, Push, Put, and Delete

---
+++ TodoListApi/Data/TodoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Data;
+
+class TodoDb(DbContextOptions<TodoDb> options) : DbContext(options)
+{
+    public DbSet<Todo> Todos => Set<Todo>();
+}
